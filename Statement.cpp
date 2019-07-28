40d5aee02dee97d16be9d8acc49b372b444600ad
Jeff Moellmer <jeff.moellmer@gmail.com>
2019-07-27 22:22:02 -0500

    Pulled functions amountFor() and volumeCredits() up out of main loop

--- Statement.cpp
+++ Statement.cpp
@@ -22,38 +22,38 @@ std::string Statement::printStatement(const nlohmann::json &invoice, const nlohm
         return plays[aPerformance["playID"].get<string>()];
     };
 
+    // function amountFor()
+    auto amountFor = [playFor](const nlohmann::json& aPerformance) {
+        int result = 0;
+        if (playFor(aPerformance)["type"] == "tragedy") {
+            result = 40000;
+            if (aPerformance["audience"].get<int>() > 30) {
+                result += 1000 * (aPerformance["audience"].get<int>() - 30);
+            }
+        } else if (playFor(aPerformance)["type"] == "comedy") {
+            result = 30000;
+            if (aPerformance["audience"].get<int>() > 20) {
+                result += 1000 + 500 * (aPerformance["audience"].get<int>() - 20);
+            }
+            result += 300 * aPerformance["audience"].get<int>();
+        } else {
+            throw logic_error("unknown type: " + playFor(aPerformance)["type"].get<string>());
+        }
+        return result;
+    };
+
+    // function volumeCredits()
+    auto volumeCreditsFor = [playFor](const nlohmann::json& aPerformance) {
+        int result = 0;
+        result += max(aPerformance["audience"].get<int>() - 30, 0);
+        if ("comedy" == playFor(aPerformance)["type"]) {
+            result += floor(aPerformance["audience"].get<int>() / 5);
+        }
+        return result;
+    };
+
     json performances = invoice["performances"];
     for (auto &perf : performances) {
-
-        // function amountFor()
-        auto amountFor = [playFor](const nlohmann::json& aPerformance) {
-            int result = 0;
-            if (playFor(aPerformance)["type"] == "tragedy") {
-                result = 40000;
-                if (aPerformance["audience"].get<int>() > 30) {
-                    result += 1000 * (aPerformance["audience"].get<int>() - 30);
-                }
-            } else if (playFor(aPerformance)["type"] == "comedy") {
-                result = 30000;
-                if (aPerformance["audience"].get<int>() > 20) {
-                    result += 1000 + 500 * (aPerformance["audience"].get<int>() - 20);
-                }
-                result += 300 * aPerformance["audience"].get<int>();
-            } else {
-                throw logic_error("unknown type: " + playFor(aPerformance)["type"].get<string>());
-            }
-            return result;
-        };
-
-        // function volumeCredits()
-        auto volumeCreditsFor = [playFor](const nlohmann::json& aPerformance) {
-            int result = 0;
-            result += max(aPerformance["audience"].get<int>() - 30, 0);
-            if ("comedy" == playFor(aPerformance)["type"]) {
-                result += floor(aPerformance["audience"].get<int>() / 5);
-            }
-            return result;
-        };
 
         volumeCredits += volumeCreditsFor(perf);
 
